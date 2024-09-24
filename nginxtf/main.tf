fb6a9cf5a5cfab96595877a110b4fcc5f30d51b0
raisalsalim <raisalsalim333@gmail.com>
2024-09-24 11:22:28 +0530

    terraform reduced

--- nginxtf/main.tf
+++ nginxtf/main.tf
@@ -1,62 +1,17 @@
-# ApplicationSet resource to deploy the Nginx app using ArgoCD
-resource "argocd_application_set" "nginx_applicationset" {
-  metadata {
-    name      = "nginx-applicationset"
-    namespace = "argocd"
+provider "helm" {
+  kubernetes {
+    host                   = var.kubernetes_host
+    token                  = var.kubernetes_token
+    cluster_ca_certificate = base64decode(var.kubernetes_ca_cert)
   }
+}
 
-  spec {
-    # Generator to generate applications for different environments
-    generator {
-      list {
-        elements = [
-          {
-            name        = "nginx-prod"
-            namespace   = "production"
-            values_file = "prod-values.yaml"
-          },
-          {
-            name        = "nginx-staging"
-            namespace   = "staging"
-            values_file = "staging-values.yaml"
-          }
-        ]
-      }
-    }
+resource "helm_release" "nginx_app" {
+  name       = "nginx-app"
+  chart      = "${path.module}/../nginxhelm"  # Path to the Helm chart
+  namespace  = var.app_namespace
 
-    # Template for deploying the Helm chart with environment-specific values
-    template {
-      metadata {
-        name      = "{{name}}"
-        namespace = "{{namespace}}"
-      }
-
-      spec {
-        project = "default"
-        
-        source {
-          repo_url        = "https://github.com/Raisal-A/repo5.git"
-          path            = "nginxhelm"  # Path to the Helm chart in repo5
-          target_revision = "HEAD"
-          helm {
-            value_files = [
-              "https://github.com/Raisal-A/repo6.git/values/{{values_file}}"  # Points to environment variables in repo6
-            ]
-          }
-        }
-
-        destination {
-          server    = "https://kubernetes.default.svc"
-          namespace = "{{namespace}}"
-        }
-
-        sync_policy {
-          automated {
-            prune     = true
-            self_heal = true
-          }
-        }
-      }
-    }
-  }
+  values = [
+    file("${path.module}/../../repo6/${var.values_file}")  # Load values from repo6
+  ]
 }
--- nginxtf/variables.tf
+++ nginxtf/variables.tf
@@ -7,3 +7,18 @@ variable "values_file" {
   description = "Environment-specific values file from repo6"
   type        = string
 }
+
+variable "kubernetes_host" {
+  description = "Kubernetes API server host"
+  type        = string
+}
+
+variable "kubernetes_token" {
+  description = "Kubernetes API access token"
+  type        = string
+}
+
+variable "kubernetes_ca_cert" {
+  description = "Base64-encoded Kubernetes CA certificate"
+  type        = string
+}
